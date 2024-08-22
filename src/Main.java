38f13450e7a0fdb9a97041a9850dd0c8b80d7fd6
Zhan Gevorgiz <zhangevorgiz@gmail.com>
2024-08-22 16:00:25 +0300

    Запуск файла - AccessLogParser.jar

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,14 @@
+import java.util.Scanner;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Случайное число от 0 до 1: " + Math.random());
+
+        System.out.println(" Введите текст нажмите Enter ");
+
+        String text = new Scanner(System.in).nextLine();
+
+        System.out.println(" Длина текста составляет " + text.length ()+" символов ");
     }
 }
